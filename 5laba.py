1a5cf95ebe97ce979185bf75b3e92328e2e6ba53
EugeneFoxWox <98107337+EugeneFoxWox@users.noreply.github.com>
2022-02-01 11:24:37 +0300

    Update 5laba.py

--- 5laba.py
+++ 5laba.py
@@ -1,1 +1,22 @@
+def sbor():
+    while True:
+        baschny = int(input('Введите кол-во башен от 3 до 10 --> '))
+        if 3 <= baschny <= 10 :
+            break
+        else:
+            print('Введите значение в заданных пределах! \n ', '-'*40)
+    return baschny
 
+
+def sbor2():
+    while True:
+        rings = int(input('Введите кол-во башен от 3 до 20 --> '))
+        if 3 <= rings <= 20:
+            break
+        else:
+            print('Введите значение в заданных пределах! \n ', '-'*40)
+    return rings
+
+
+sbor()
+sbor2()
